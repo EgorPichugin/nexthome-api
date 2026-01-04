26c33e554bf43fdef2d765fc7f2e18407c0a0a22
egor <pichugin.egor1993@gmail.com>
2026-01-04 14:27:13 +0100

    * environment manager replaced with environment options

--- NextHome.API/Program.cs
+++ NextHome.API/Program.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using NextHome.API.Extensions;
 
@@ -9,14 +10,17 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        var envOptions = builder.Configuration.Get<EnvironmentOptions>()
+                         ?? throw new InvalidOperationException("EnvironmentOptions not found");
+        
         builder.Services.ConfigureServices(builder.Configuration);
         
         if (builder.Environment.IsDevelopment())
         {
-            builder.Services.ConfigureCors();
+            builder.Services.ConfigureCors(envOptions);
         }
         var app = builder.Build();
-        app.ConfigureApp();
+        app.ConfigureApp(envOptions);
         app.Run();
     }
 }
